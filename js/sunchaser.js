e449bdd92cef08d1f4304eb86e3f5acbe1b2f285
cnlmx <42601081+cnlmx@users.noreply.github.com>
2021-08-31 13:49:33 +0800

    Site updated: 2021-08-31 13:49:33

--- js/sunchaser.js
+++ js/sunchaser.js
@@ -1,1 +1,26 @@
-document.getElementById("darkmode").addEventListener("click",(function(e){"light"===("dark"===document.documentElement.getAttribute("data-theme")?"dark":"light")?(activateDarkMode(),saveToLocal.set("theme","dark",2),void 0!==GLOBAL_CONFIG.Snackbar&&btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)):(activateLightMode(),saveToLocal.set("theme","light",2),void 0!==GLOBAL_CONFIG.Snackbar&&btf.snackbarShow(GLOBAL_CONFIG.Snackbar.night_to_day)),"function"==typeof utterancesTheme&&utterancesTheme(),"object"==typeof FB&&window.loadFBComment(),window.DISQUS&&document.getElementById("disqus_thread").children.length&&setTimeout(()=>window.disqusReset(),200)}));var isMobile=/Android|webOS|iPhone|BlackBerry/i.test(navigator.userAgent);
+document.getElementById('darkmode').addEventListener('click', function (e) {
+  const nowMode = document.documentElement.getAttribute('data-theme') === 'dark' ? 'dark' : 'light'
+  if (nowMode === 'light') {
+    activateDarkMode()
+    saveToLocal.set('theme', 'dark', 2)
+    GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)
+  } else {
+    activateLightMode()
+    saveToLocal.set('theme', 'light', 2)
+    GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.night_to_day)
+  }
+  // handle some cases
+  typeof utterancesTheme === 'function' && utterancesTheme()
+  typeof FB === 'object' && window.loadFBComment()
+  window.DISQUS &&
+    document.getElementById('disqus_thread').children.length &&
+    setTimeout(() => window.disqusReset(), 200)
+})
+
+// 判断是手机端还是电脑端
+var isMobile = /Android|webOS|iPhone|BlackBerry/i.test(navigator.userAgent)
+if (isMobile) {
+  // 当前网页在手机端打开
+} else {
+  // 当前网页在PC端打开
+}
